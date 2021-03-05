0d9bfb13b46a2422378e7b2ff33f3f5740666e9a
Ensar KURT <37beykozlu37@gmail.com>
2021-03-06 01:13:41 +0300

    fixed static field

--- .history/archive_20201203213526.py
+++
@@ -1,6 +0,0 @@
-from selenium.webdriver.common.keys import Keys
-drv= webdriver.Chrome()
-drv.get('https://www.instagram.com/accounts/login/?source=auth_switcher')
-drv.find_element_by_name("username").send_keys('your_username_here')
-drv.find_element_by_name("password").send_keys('your_password_here') 
-drv.find_element_by_name("password").send_keys(u'\ue007')
